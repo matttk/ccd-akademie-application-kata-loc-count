bfe63ecaacf940259a3769202802b7c3ce3192e9
Matthew Kirschnick <matthew.kirschnick@node.energy>
2021-12-14 13:00:48 +0100

    Basic outline

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
+function getOriginDirectory() {
+  return "./";
+}
+
+function getDirectoryInfo(originDirectory) {
+  const files = getFiles(originDirectory);
+  const directories = getDirectories(originDirectory);
+
+  const filesInfo = files.map(getFileInfo);
+  const directoriesInfo = directories.map(getDirectoryInfo);
+
+  return [...filesInfo, ...directoriesInfo];
+}
+
+function getFiles() {
+  return ["./index.js"];
+}
+
+function getDirectories() {
+  return [];
+}
+
+function getFileInfo() {
+  return {
+    path: "./index.js",
+    lines: 10,
+    linesOfCode: 5,
+  };
+}
+
+function createResults(directoryInfo) {
+  return directoryInfo.map(createResult);
+}
+
+function createResult(fileInfo) {
+  return `${fileInfo.path} ${fileInfo.lines} ${fileInfo.linesOfCode}`;
+}
+
+function displayResults(results) {
+  results.forEach(displayResult);
+}
+
+function displayResult(result) {
+  console.log(result);
+}
+
 function main() {
-  console.log("hello world");
+  const originDirectory = getOriginDirectory();
+  const directoryInfo = getDirectoryInfo(originDirectory);
+  const results = createResults(directoryInfo);
+
+  displayResults(results);
 }
 
 main();
