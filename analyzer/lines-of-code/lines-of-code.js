d29274b51fa042f6f2b20afd129be0c108a2e334
Matthew Kirschnick <matthew.kirschnick@node.energy>
2021-12-14 16:01:39 +0100

    Do not count simple comments as a line of code

--- analyzer/lines-of-code/lines-of-code.js
+++ analyzer/lines-of-code/lines-of-code.js
@@ -1,7 +1,9 @@
+const COMMENT_SYMBOL = "//";
+
 function countLines(filename, data) {
   const lines = splitStringByNewlines(data);
   const numLines = lines.length;
-  const linesOfCode = lines.filter((line) => line.trim() !== "");
+  const linesOfCode = lines.filter(isLineOfCode);
   const numLinesOfCode = linesOfCode.length;
 
   return {
@@ -13,6 +15,10 @@ function countLines(filename, data) {
 
 function splitStringByNewlines(string) {
   return string.split("\n");
+}
+
+function isLineOfCode(line) {
+  return line.trim() !== "" && line.indexOf(COMMENT_SYMBOL) !== 0;
 }
 
 export { countLines };
